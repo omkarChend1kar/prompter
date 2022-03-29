d16d109cf14f1f7a83b28b29d028880ac2c52a23
Omkar Chendwankar <omkarchendwankar45@gmail.com>
2022-03-29 16:31:33 +0530

    Removed unnecessary comments in example file

--- prompter/example/main.dart
+++ prompter/example/main.dart
@@ -2,11 +2,6 @@ import 'dart:io';
 import 'package:prompter_oc/prompter_oc.dart';
 
 void main() {
-  // var terminal = Terminal();
-  // terminal.clearScreen();
-  // terminal.printPrompt("Hello World");
-  // var printInput = terminal.collectInput();
-  // stdout.writeln('You just Entered : ${printInput}');
   final option = [
     new Option('Red', '#f00'),
     new Option('Green', '#f01'),
